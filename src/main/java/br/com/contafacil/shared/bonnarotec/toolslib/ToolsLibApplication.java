75e7c98b4e7794584d4ef97ddc32c1dd675a1cb9
Lucas Bonna Favaro <88051985+LucasBonna@users.noreply.github.com>
2024-11-01 17:17:16 -0300

    fix: test

--- src/main/java/br/com/contafacil/shared/bonnarotec/toolslib/ToolsLibApplication.java
+++ src/main/java/br/com/contafacil/shared/bonnarotec/toolslib/ToolsLibApplication.java
@@ -1,9 +1,7 @@
 package br.com.contafacil.shared.bonnarotec.toolslib;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
 public class ToolsLibApplication {
 
 	public static void main(String[] args) {
--- src/test/java/br/com/contafacil/shared/bonnarotec/toolslib/ToolsLibApplicationTests.java
+++ src/test/java/br/com/contafacil/shared/bonnarotec/toolslib/ToolsLibApplicationTests.java
@@ -1,9 +1,7 @@
 package br.com.contafacil.shared.bonnarotec.toolslib;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class ToolsLibApplicationTests {
 
 	@Test
