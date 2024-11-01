2307d78be57d522f5fa64cb26d6abb9274f39a15
Lucas Bonna Favaro <88051985+LucasBonna@users.noreply.github.com>
2024-11-01 16:17:11 -0300

    first commit

---
+++ src/main/java/br/com/contafacil/shared/bonnarotec/toolslib/ToolsLibApplication.java
@@ -0,0 +1,13 @@
+package br.com.contafacil.shared.bonnarotec.toolslib;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToolsLibApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToolsLibApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/contafacil/shared/bonnarotec/toolslib/ToolsLibApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.contafacil.shared.bonnarotec.toolslib;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToolsLibApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
