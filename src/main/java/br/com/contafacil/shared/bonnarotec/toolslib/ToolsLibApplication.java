abacfdf22bb7b9e5b9f88a3395b88cff91ead042
Lucas Bonna Favaro <88051985+LucasBonna@users.noreply.github.com>
2024-11-01 22:16:57 -0300

    fix: jpa2

--- src/main/java/br/com/contafacil/shared/bonnarotec/toolslib/ToolsLibApplication.java
+++ src/main/java/br/com/contafacil/shared/bonnarotec/toolslib/ToolsLibApplication.java
@@ -1,14 +1,7 @@
 package br.com.contafacil.shared.bonnarotec.toolslib;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 
-@SpringBootApplication(exclude = {
-		HibernateJpaAutoConfiguration.class,
-		DataSourceAutoConfiguration.class
-})
 public class ToolsLibApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/br/com/contafacil/shared/bonnarotec/toolslib/config/JpaConfig.java
@@ -0,0 +1,18 @@
+package br.com.contafacil.shared.bonnarotec.toolslib.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+
+@Configuration
+public class JpaConfig {
+
+    @Bean
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
+        LocalContainerEntityManagerFactoryBean em = new LocalContainerEntityManagerFactoryBean();
+        em.setJpaVendorAdapter(new HibernateJpaVendorAdapter());
+        em.setPackagesToScan("br.com.contafacil.shared.bonnarotec.toolslib.domain");
+        return em;
+    }
+}
