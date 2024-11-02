c0440bcd56d639952c88adf96b2744944ba6aeb7
Lucas Bonna Favaro <88051985+LucasBonna@users.noreply.github.com>
2024-11-01 22:34:35 -0300

    fix: jpa connection

--- src/main/java/br/com/contafacil/shared/bonnarotec/toolslib/ToolsLibApplication.java
+++ src/main/java/br/com/contafacil/shared/bonnarotec/toolslib/ToolsLibApplication.java
@@ -1,10 +1,15 @@
 package br.com.contafacil.shared.bonnarotec.toolslib;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
 public class ToolsLibApplication {
 
-	public static void main( String[] args )
-	{
-		System.out.println( "Hello World!" );
+	public static void main(String[] args) {
+		SpringApplication.run(ToolsLibApplication.class, args);
 	}
 
 }
