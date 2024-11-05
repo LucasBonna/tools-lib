06ae7832b261a8b87306316b12c713e09bceb6c9
Lucas Bonna Favaro <88051985+LucasBonna@users.noreply.github.com>
2024-11-05 13:30:18 -0300

    feat: file entity

---
+++ src/main/java/br/com/contafacil/shared/bonnarotec/toolslib/domain/file/FileEntity.java
@@ -0,0 +1,45 @@
+package br.com.contafacil.shared.bonnarotec.toolslib.domain.file;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "files")
+public class FileEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(nullable = false)
+    private String fileName;
+
+    @Column(nullable = false)
+    private String extension;
+
+    @Column(nullable = false)
+    private String contentType;
+
+    @Column(nullable = false)
+    private String filePath;
+
+    @Column(name = "created_at", nullable = false)
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at", nullable = false)
+    private LocalDateTime updatedAt;
+
+    @Column(name = "deleted_at", nullable = true)
+    private LocalDateTime deletedAt;
+
+}
